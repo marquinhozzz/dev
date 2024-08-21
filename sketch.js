e55ebbe3e318c8136387a1a64e7e0ce822531bed
marquinhozzz <lins.marcos@escola.pr.gov.br>
2024-08-21 16:36:03 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }
+  
+  
+  function draw() {
+    
+    stroke ("blue");
+    fill ("red");
+   
+    
+    // console.log(mouseIsPressed);
+    
+    if (mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
